62d6d5f5b0bb69be5310ebb87e63398990d9a1d1
I54m <i54m@i54m.com>
2025-12-15 00:06:03 +1300

    Initial commit

---
+++ src/main/java/net/vorplex/communitygoals/VorplexCommunityGoals.java
@@ -0,0 +1,44 @@
+package net.vorplex.communitygoals;
+
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VorplexCommunityGoals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        long startTime = System.nanoTime();
+        getComponentLogger().info("");
+        getComponentLogger().info(Component.text("██╗   ██╗ ██████╗ ██████╗ ██████╗ ██╗     ███████╗██╗  ██╗").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text("██║   ██║██╔═══██╗██╔══██╗██╔══██╗██║     ██╔════╝╚██╗██╔╝").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text("██║   ██║██║   ██║██████╔╝██████╔╝██║     █████╗   ╚███╔╝").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text("╚██╗ ██╔╝██║   ██║██╔══██╗██╔═══╝ ██║     ██╔══╝   ██╔██╗").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text(" ╚████╔╝ ╚██████╔╝██║  ██║██║     ███████╗███████╗██╔╝ ██╗").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text("  ╚═══╝   ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝").color(NamedTextColor.LIGHT_PURPLE));
+        getComponentLogger().info(Component.text("       ___  __                    __   __   __   ___").color(NamedTextColor.DARK_PURPLE));
+        getComponentLogger().info(Component.text("        |  /  \\ |  | |\\ | \\ /    /  ` /  \\ |__) |__ ").color(NamedTextColor.DARK_PURPLE));
+        getComponentLogger().info(Component.text("        |  \\__/ |/\\| | \\|  |     \\__, \\__/ |  \\ |___").color(NamedTextColor.DARK_PURPLE));
+        getComponentLogger().info("───────────────────────────────────────────────────────────");
+        getComponentLogger().info(Component.text("Developed by I54m").color(NamedTextColor.RED));
+        getComponentLogger().info(Component.text("v" + getPluginMeta().getVersion() + " Running on " + getServer().getVersion()).color(NamedTextColor.RED));
+        getComponentLogger().info("───────────────────────────────────────────────────────────");
+        getComponentLogger().info(Component.text("Registering Commands...").color(NamedTextColor.GREEN));
+        this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+
+        });
+        getComponentLogger().info(Component.text("Registered Commands!").color(NamedTextColor.GREEN));
+        getComponentLogger().info(Component.text("Registering event listeners...").color(NamedTextColor.GREEN));
+
+        getComponentLogger().info(Component.text("Registered event listeners!").color(NamedTextColor.GREEN));
+        getComponentLogger().info(Component.text("Plugin loaded in: " + (System.nanoTime() - startTime) / 1000000 + "ms!").color(NamedTextColor.GREEN));
+        getComponentLogger().info("───────────────────────────────────────────────────────────");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/net/vorplex/communitygoals/plugin/PaperBootstrap.java
@@ -0,0 +1,23 @@
+package net.vorplex.communitygoals.plugin;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import net.vorplex.communitygoals.VorplexCommunityGoals;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaperBootstrap implements PluginBootstrap {
+    @Override
+    public void bootstrap(BootstrapContext context) {
+//        context.getLogger().info(Component.text("Registering Commands..."));
+//        context.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+//            commands.registrar().register(GiveCommand.COMMAND_NODE);
+//        });
+//        context.getLogger().info(Component.text("Registered Commands!"));
+    }
+
+    @Override
+    public JavaPlugin createPlugin(PluginProviderContext context) {
+        return new VorplexCommunityGoals();
+    }
+}

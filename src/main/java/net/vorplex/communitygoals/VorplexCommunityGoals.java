2bab21b6b32df1b2ade0be982cee3f97d6769580
I54m <i54m@i54m.com>
2025-12-16 01:21:54 +1300

    Implement Goal save/load logic and command registration

--- src/main/java/net/vorplex/communitygoals/VorplexCommunityGoals.java
+++ src/main/java/net/vorplex/communitygoals/VorplexCommunityGoals.java
@@ -1,11 +1,25 @@
 package net.vorplex.communitygoals;
 
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import lombok.Getter;
+import lombok.Setter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.vorplex.communitygoals.commands.ContributeCommand;
+import net.vorplex.communitygoals.goals.GoalController;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class VorplexCommunityGoals extends JavaPlugin {
+
+    @Getter @Setter
+    private static VorplexCommunityGoals instance;
+
+    public final File GOALS_DATA_DIRECTORY = new File(getDataFolder(), "goals-data");
+    public final GoalController GOAL_CONTROLLER = new GoalController(this);
+    public final FileConfiguration CONFIG = getConfig();
 
     @Override
     public void onEnable() {
@@ -23,14 +37,18 @@ public final class VorplexCommunityGoals extends JavaPlugin {
         getComponentLogger().info(Component.text("                   __   __             __         ").color(NamedTextColor.DARK_PURPLE));
         getComponentLogger().info(Component.text("                  / _` /  \\  /\\  |    /__`        ").color(NamedTextColor.DARK_PURPLE));
         getComponentLogger().info(Component.text("                  \\__> \\__/ /~~\\ |___ .__/        ").color(NamedTextColor.DARK_PURPLE));
-
         getComponentLogger().info("───────────────────────────────────────────────────────────");
         getComponentLogger().info(Component.text("Developed by I54m").color(NamedTextColor.RED));
         getComponentLogger().info(Component.text("v" + getPluginMeta().getVersion() + " Running on " + getServer().getVersion()).color(NamedTextColor.RED));
         getComponentLogger().info("───────────────────────────────────────────────────────────");
+        setInstance(this);
+        if (!new File(getDataFolder(), "config.yml").exists())
+            saveDefaultConfig();
+        GOAL_CONTROLLER.LoadGoals();
+        //todo scheduled task to check goals for completions and check end time
         getComponentLogger().info(Component.text("Registering Commands...").color(NamedTextColor.GREEN));
         this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
-
+            commands.registrar().register(ContributeCommand.COMMAND_NODE);
         });
         getComponentLogger().info(Component.text("Registered Commands!").color(NamedTextColor.GREEN));
         getComponentLogger().info(Component.text("Registering event listeners...").color(NamedTextColor.GREEN));
@@ -43,6 +61,6 @@ public final class VorplexCommunityGoals extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        GOAL_CONTROLLER.SaveGoals();
     }
 }

9ef4cb3da9229e34221830b49f781d7e7c5a993a
I54m <i54m@i54m.com>
2025-12-16 01:20:44 +1300

    Add GoalData Object

---
+++ src/main/java/net/vorplex/communitygoals/goals/GoalData.java
@@ -0,0 +1,31 @@
+package net.vorplex.communitygoals.goals;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+public class GoalData {
+
+    @Getter
+    private final String goalID;
+    @Getter
+    private final Map<Material, Integer> currentItems = new HashMap<>();
+    @Getter
+    private final Map<UUID, PlayerContributions> contributors = new HashMap<>();
+    @Getter @Setter
+    private boolean completed;
+
+    public GoalData(@NotNull String GoalID, boolean completed, @NotNull Map<Material, Integer> currentItems, @NotNull Map<UUID, PlayerContributions> contributors) {
+        this.goalID = Objects.requireNonNull(GoalID);
+        this.completed = completed;
+        this.currentItems.putAll(Objects.requireNonNull(currentItems));
+        this.contributors.putAll(Objects.requireNonNull(contributors));
+    }
+
+}

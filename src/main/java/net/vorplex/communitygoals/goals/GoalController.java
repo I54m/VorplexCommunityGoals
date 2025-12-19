8cccf6bfdaf903fb516d5295ee3ac609e349bf71
I54m <i54m@i54m.com>
2025-12-19 17:55:53 +1300

    Add TODO

--- src/main/java/net/vorplex/communitygoals/goals/GoalController.java
+++ src/main/java/net/vorplex/communitygoals/goals/GoalController.java
@@ -73,6 +73,7 @@ public class GoalController {
         PLUGIN.getComponentLogger().info(Component.text("Attempting to load Goals from config...").color(NamedTextColor.GREEN));
         Objects.requireNonNull(PLUGIN.CONFIG.getConfigurationSection("goals")).getKeys(false).forEach(goalID -> {
             Map<Material, Integer> reqItems = new HashMap<>();
+            //TODO check Material is a valid material
             Objects.requireNonNull(PLUGIN.getConfig().getConfigurationSection("goals." + goalID + ".required-items")).getKeys(false).forEach(itemID ->
                 reqItems.put(Material.getMaterial(itemID), PLUGIN.getConfig().getInt("goals." + goalID + ".required-items." + itemID))
             );
